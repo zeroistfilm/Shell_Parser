0f827282ce4855d5603ac473e211eab9e9acdb17
YD <zeroistfilm@naver.com>
2022-10-08 01:35:12 +0900

    add serverInfo (Ip, Location)

--- main.py
+++ main.py
@@ -3,6 +3,25 @@ import subprocess
 import json
 from csv import DictWriter
 import datetime
+import requests
+
+class ServerInfo:
+
+    def get_ip(self):
+        response = requests.get('https://api.ipify.org?format=json').json()
+        return response["ip"]
+
+    def get_location(self):
+        ip_address = self.get_ip()
+        response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+        location_data = {
+            "ip": ip_address,
+            "city": response.get("city"),
+            "region": response.get("region"),
+            "country": response.get("country_name")
+        }
+        return location_data
+
 
 
 class GameDB:
@@ -83,8 +102,7 @@ class FlowLog:
                          'other_bytes',
                          'other_packets',
                          'total_bytes',
-                         'total_packets'
-                         ]
+                         'total_packets']
 
         self.resultData = {}
 
