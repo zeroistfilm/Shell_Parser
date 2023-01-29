a0b9b12c4b682e40e9ec5cbc315221d22e0c0f61
YD <zeroistfilm@naver.com>
2023-01-29 19:02:42 +0900

    log -> dev/null

--- restart_consumer.sh
+++ restart_consumer.sh
@@ -1,5 +1,9 @@
 kill -9 $(ps -ef | grep consumer | awk '{print $2}')
 rm Mareel_GO_log.out Mareel_PRO_log.out Mareel_VPN_log.out
-nohup python3 consumer_GO.py &> Mareel_GO_log.out &
-nohup python3 consumer_PRO.py &> Mareel_PRO_log.out &
-nohup python3 consumer_VPN.py &> Mareel_VPN_log.out &
+nohup python3 consumer_GO.py 1> /dev/null 2>&1 &
+nohup python3 consumer_PRO.py 1> /dev/null 2>&1 &
+nohup python3 consumer_VPN.py 1> /dev/null 2>&1 &
+
+#nohup python3 consumer_GO.py &> Mareel_GO_log.out &
+#nohup python3 consumer_PRO.py &> Mareel_PRO_log.out &
+#nohup python3 consumer_VPN.py &> Mareel_VPN_log.out &
