1bf3e8f9c0ea2de793a408de1dde66c2db8ef698
Henry Choi <wschd770@gmail.com>
2024-03-07 00:30:42 +0900

    Try to fix DB connection error
    Signed-off-by: Henry Choi <wschd770@gmail.com>

--- consumer.py
+++ consumer.py
@@ -59,8 +59,8 @@ async def consume(service, topic, queue):
 async def saver(queue):
     print("start saver")
     global mareeldb
-    mareeldb = mareelDB(useLocalDB=True)
-    while True:
+    mareeldb = mareelDB(useLocalDB=False)
+    while False:
         try:
             bulk = await queue.get()
             mareeldb.session.add_all(bulk)
